9170e659e6012fe966021a3b0d77338f318e80c8
Vadim <mr.santa.desert@gmail.com>
2026-01-07 19:03:24 +0300

    added works component

---
+++ src/components/Works.vue
@@ -0,0 +1,39 @@
+<script></script>
+<template>
+<div class="relative bg-[#292F36]">
+    <img src="../assets/images/background/skills.png" alt="Bg with faces" class="absolute z-1 object-cover w-full h-full top-0 left-0">
+    <section class="w-416 my-0 mx-auto">
+        <div class="z-1 relative p-32 flex flex-col gap-16">
+            <div class="z-1 flex flex-col gap-16">
+                <div class="flex flex-col justify-center items-center gap-4">
+                    <img src="../assets/icons/common/mouse.svg" alt="Mouse icon" class="w-8 h-11 animate-float">
+                    <div class="w-0 h-20 border border-dashed border-white"></div>
+                </div>
+                <div class="flex flex-col gap-4">
+                    <h1 class="text-white font-family-ubuntu text-[64px] leading-18 tracking-normal text-center capitalize">
+                        <span class="text-main">Works</span><br>
+                        <span class="text-white font-family-ibm text-[16px] leading-5 tracking-normal text-center">I had the pleasure of working with these awesome projects</span>
+                    </h1>
+                </div>
+            </div>
+            <div class="flex flex-row justify-center">
+                <div class="flex items-center justify-center gap-16">
+                    <button class="w-18 h-18 flex flex-row justify-start items-center p-4.5 rounded-[48px] bg-[#35353A] hover:scale-110 transition-[100,100]">
+                        <img src="../assets/icons/common/icon-chevron-left.svg" alt="Левая кнопка" class="left-chevron">
+                    </button>
+                    <div class="flex justify-center items-baseline">
+                        <div class="flex items-end gap-2 absolute mt-15.5 mr-29 mb-84.25 ml-89.75">
+                            <a href="#" class="inline-block border-b border-solid border-white text-main font-family-ibm text-2xl leading-8 tracking-normal capitalize hover:scale-105 transition-[100,100]">View Website</a>
+                            <img src="../assets/icons/common/icon-mouse-pointer.svg" alt="Иконка курсора" class="icon-coursor">
+                        </div>
+                        <img src="../assets/images/photos/frame1.png" alt="Изображение монитора" class="dual-screen">
+                    </div>
+                    <button class="w-18 h-18 flex flex-row justify-start items-center p-4.5 rounded-[48px] bg-[#35353A] hover:scale-110 transition-[100,100]">
+                        <img src="../assets/icons/common/icon-chevron-right.svg" alt="Правая кнопка" class="right-chevron">
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+</div> 
+</template>
